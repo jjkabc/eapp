6437849c451c77390899175a0947943183cb06cc
Samuel Besong <besongsamuel@yahoo.com>
2017-08-04 14:41:03 -0400

    Update Blog_model.php

--- application/models/Blog_model.php
+++ application/models/Blog_model.php
@@ -22,14 +22,51 @@ class Blog_model extends CI_Model
 	*/
     public function get_recent_stat_posts()
     {
-        return null;
+        return $this->get_recent_posts(POST_TYPE_STAT);
     }
 	
 	/**
 	* Get all recent posts 
 	*/
-	public function get_recent_posts()
+	public function get_recent_posts($type = -1)
 	{
-		return null;
+		$result_array = array();
+		
+		if($type = -1)
+		{
+			$this->db->where(array("type" => $type));
+			$result = $this->db->get(BLOG_POSTS);
+		}
+		else
+		{
+			$result = $this->db->get(BLOG_POSTS);
+		}
+		
+		foreach($result->result() as $post)
+		{
+			// get post comments
+			$post->comments = $this->get_post_data($post->id, BLOG_POSTS_COMMENTS);
+			
+			// get post likes
+			$post->likes = $this->get_post_data($post->id, BLOG_POSTS_LIKES);;
+			
+			$result_array[$post->id] = $post;
+		}
+		
+		return $result_array;
+	}
+	
+	private function get_post_data($post_id, $table_name)
+	{
+		$result_array = array();
+		$this->db->where(array("post_id" => $post_id));
+		$res = $this->db->get($table_name);
+		
+		foreach($res->result() as $data)
+		{
+			$result_array[$data->id] = $data;
+		}
+		
+		return $result_array;
 	}
 }
