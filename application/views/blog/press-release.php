08792745044f13f65e66fbf782f26a0e233c43b9
Samuel Besong <besongsamuel@yahoo.com>
2017-08-04 14:49:24 -0400

    Update press-release.php

--- application/views/blog/press-release.php
+++ application/views/blog/press-release.php
@@ -1,83 +1,34 @@
 <!DOCTYPE html>
 
     <!-- page builer -->
-    <section class="section-white clearfix blog" ng-controller="BlogController">
+    <section id="blog-container" class="section-white clearfix blog" ng-controller="BlogController">
         <div class="container">
             <div class="row">
                 <div class="pull-left col-md-8 col-sm-8 col-xs-12">
                     <div id="blog-page" class="row clearfix">
-                        <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn first">
+                        <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn" ng-class="$index % 2 == 0 ? 'first' : 'last'" ng-repeat="post in RecentPosts">
                             <div class="blog-item">
                                 <div class="ImageWrapper">
-                                    <img src="<?php echo base_url("assets/blog/"); ?>img/epicerie.png" alt="" class="img-responsive">
+                                    <img src="<?php echo base_url("assets/blog/"); ?>img/posts/{{post.image}}" alt="" class="img-responsive">
                                     <div class="ImageOverlayLi"></div>
                                     <div class="Buttons StyleH">
-                                        <a href="#" title="Like it"><span class="bubble border-radius"><i class="fa fa-heart-o"></i> 3</span></a>
-                                        <a href="blog-20 façons simples d'économiser.html" title="Permalink to : My post example title goes here"><span class="bubble border-radius"><i class="fa fa-comment-o"></i> 7</span></a>
+                                        <a ng-show="canLike() && userLogged" href ng-click="like(post.id)" title="Aimer"><span class="bubble border-radius"><i class="fa fa-heart-o"></i> {{post.likes}}</span></a>
+										<a ng-show="!canLike() && userLogged" href ng-click="like(post.id)" title="Pas aimer"><span class="bubble border-radius"><i class="fa fa-thumbs-down"></i></span></a>
+                                        <a href="<?php echo base_url("blog/comments/")?>{{post.id}}" title="Voir Commentaires"><span class="bubble border-radius"><i class="fa fa-comment-o"></i>{{post.comments.length}}</span></a>
                                     </div>
                                 </div>
                                 <div class="meta">
-                                    <span><a href="#">INFOS</a> | 02-Jan-2015</span>
+                                    <span><a href >INFOS</a> | {{post.date | date}}</span>
                                 </div><!-- end meta -->
                                 <div class="blog-title">
-                                    <h3><a href="blog-2economiser.html" title="">20 façons simples d'économiser sur votre prochaine facture d'épicerie</a></h3>
+                                    <h3><a href="<?php echo base_url("blog/view/")?>{{post.id}}" title="">{{post.title}}</a></h3>
                                 </div><!-- end title -->
                                 <div class="blog-desc">
-                                    <p>Faire son épicerie, ça coûte cher. Il est tellement facile d’y entrer pour quelques produits et d’y ressortir avec un portefeuille vide. Puisque nous devons tous nous nourrir, pas le choix, il vaut mieux privilégier les repas faits maison, qui coûtent moins cher et sont plus nourrissants.</p>
+                                    <p>{{post.description}}</p>
                                 </div><!-- end desc -->
                                 <div class="blog-button">
-                                    <a href="blog-economiser.html" title="" class="btn btn-primary border-radius">Lire</a>
+                                    <a href="<?php echo base_url("blog/comments/")?>{{post.id}} title="" class="btn btn-primary border-radius">Lire</a>
                                 </div><!-- end button -->
-                            </div><!-- end blog -->
-                        </div><!-- end col -->
-
-                        <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn last">
-                            <div class="blog-item">
-                                <div class="ImageWrapper">
-                                    <img src="<?php echo base_url("assets/blog/"); ?>img/epicerie2.png" alt="" class="img-responsive">
-                                    <div class="ImageOverlayLi"></div>
-                                    <div class="Buttons StyleH">
-                                        <a href="#" title="Like it"><span class="bubble border-radius"><i class="fa fa-heart-o"></i> 3</span></a>
-                                        <a href="blog-dollarama.html" title="Permalink to : My post example title goes here"><span class="bubble border-radius"><i class="fa fa-comment-o"></i> 7</span></a>
-                                    </div>
-                                </div>
-                                <div class="meta">
-                                    <span><a href="#">INFOS</a> | 02-Jan-2015</span>
-                                </div><!-- end meta -->
-                                <div class="blog-title">
-                                    <h3><a href="blog-single-sidebar-one.html" title="">Dollarama et l’épicerie à très bas prix</a></h3>
-                                </div><!-- end title -->
-                                <div class="blog-desc">
-                                    <p>Alors que le prix d’un panier type d’épicerie a grimpé de près de 12 % en deux ans, Le Journal a constaté que les consommateurs peuvent économiser de 32 % à 45 % s’ils font une partie de leur épicerie chez... Dollarama. Sur une liste de 11 articles, identiques ou équivalents selon le commerce, la facture totale varie entre 23,50 $ chez Dollarama, et 43,19 $ chez Provigo. Si plusieurs consommateurs croient qu’il n’y a que du «cannage» et des bonbons au Dollarama, détrompez-vous. De nombreux produits de marque, également offerts dans les supermarchés, s’y trouvent à des prix jusqu’à deux fois moins élevés. Il y a notamment du beurre d’arachides naturel, de l’édulcorant à base de feuilles de stévia et du gruau de marque connue. Offre complémentaire. </p>
-                                </div><!-- end desc -->
-                                <div class="blog-button">
-                                    <a href="blog-dollarama.html" title="" class="btn btn-primary border-radius">Lire</a>
-                                </div><!-- end button -->
-                            </div><!-- end blog -->
-                        </div><!-- end col -->
-
-                        <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn first">
-                            <div class="blog-item">
-                                <div class="ImageWrapper">
-                                    <img src="<?php echo base_url("assets/blog/"); ?>img/epicerie3.jpg" alt="" class="img-responsive">
-                                    <div class="ImageOverlayLi"></div>
-                                    <div class="Buttons StyleH">
-                                        <a href="#" title="Like it"><span class="bubble border-radius"><i class="fa fa-heart-o"></i> 3</span></a>
-                                        <a href="blog-enquete.html" title="Permalink to : My post example title goes here"><span class="bubble border-radius"><i class="fa fa-comment-o"></i> 7</span></a>
-                                    </div>
-                                </div>
-                               <div class="meta">
-                                            <span><a href="#">INFOS</a> | 02-Jan-2015</span>
-                                        </div><!-- end meta -->
-                                        <div class="blog-title">
-                                            <h3><a href="blog-single-sidebar-one.html" title="">ENQUÊTE DE PRIX : NOS RÉSULTATS</a></h3>
-                                        </div><!-- end title -->
-                                        <div class="blog-desc">
-                                            <p>Dans quelle épicerie et dans quelle région sont les aliments les moins chers? Est-il avantageux de courir les rabais de supermarché en supermarché? Les marques maison en valent-elles toujours la peine? Voici ce que révèle notre enquête. Par un heureux hasard, IGA a lancé sa campagne de changement de prix quelques jours après le début de notre grand magasinage, ce qui nous a permis de faire d’intéressantes comparaisons. Nos premières visites ont eu lieu avant la réduction annoncée, et les autres, après. En moyenne, le prix du panier contenant des produits de marques nationales a diminué de 7$ après l’annonce, et celui des marques économiques, de 14$.</p>
-                                        </div><!-- end desc -->
-                                        <div class="blog-button">
-                                            <a href="blog-enquete.html" title="" class="btn btn-primary border-radius">Lire</a>
-                                        </div><!-- end button -->
                             </div><!-- end blog -->
                         </div><!-- end col -->
                     </div><!-- end blog -->
@@ -102,4 +53,12 @@
         </div><!-- end row -->
     </div><!-- end container -->
 </section><!-- end section white -->
+
+<script>
+	var scope = angular.element($("#blog-container")).scope();
+	scope.$apply(function()
+ 	{
+		scope.recentPosts = JSON.Parse('<?php echo $recentPosts; ?>');
+	});
+</script>
 
