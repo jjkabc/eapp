052e2ea3d88ed3bc41d7a1d31f7b60f1b4f8dcd6
Samuel Besong <besongsamuel@yahoo.com>
2017-08-04 08:45:58 -0400

    Update Blog.php

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -5,6 +5,8 @@ class Blog extends CI_Controller {
 
      public function __construct()
     {
+		$this->data['css'] = $this->load->view('blog/css', $this->data, TRUE);
+		$this->data['scripts'] = $this->load->view('blog/scripts', $this->data, TRUE);
         parent::__construct();
     }
     
