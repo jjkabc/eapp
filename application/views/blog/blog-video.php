9e0268e18312df92b41355e4aff5a03cee87b3b3
besongsamuel <besongsamuel@yahoo.com>
2017-08-04 08:08:06 -0400

    Started implementing the blog

---
+++ application/views/blog/blog-video.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Épicerie - Video</title>
+    
+    <!-- PORTFOLIO AND LIGHTBOX SETTINGS -->
+    <link href="bfassets/css/bootFolio.css" rel="stylesheet">
+    <link href="bfassets/css/prettyPhoto.css" rel="stylesheet">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/et-icons.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/owl-carousel.css" rel="stylesheet">
+    <link href="css/flexslider.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <!-- Custom font -->
+    <link href='http://font.googleapis.com/css?family=Source+Sans+Pro:400,300,600,400italic,600italic,700,700italic,900' rel='stylesheet' type='text/css'>
+    <link href='http://font.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+
+    <header id="header-one" class="header">
+        <div class="container-fluid">
+            <div class="menu-wrapper">
+                <nav id="navigation" class="navbar yamm" role="navigation">
+                    <div class="navbar-inner">  
+                        <div id="navbar-collapse" class="navbar-right navbar-collapse collapse clearfix">
+                            <ul class="nav navbar-nav">
+                               <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blogue</a>
+                                    <ul class="dropdown-menu" role="menu">
+										<li><a href="blog-epicerie.html">L'épicerie dans la presse</a></li>
+										<li><a href="blog-statistique.html">Statistique</a></li>
+										<li><a href="blog-video.html">Video</a></li>
+                                    </ul>
+                                </li>
+                            </ul><!-- end navbar-right -->
+                        </div><!-- end navbar-callopse -->
+                    </div><!-- end navbar-inner -->
+                </nav><!-- end navigation -->
+            </div><!-- menu wrapper -->
+            <div class="shadow"></div>
+        </div>
+    </header><!-- end header -->
+
+        <section class="page-white bgpatttern clearfix">
+            <div class="section-title">
+                <div class="breadcrumb-container">
+                    <ul class="breadcrumb">
+                        <li><a href="index.html">Acceuil</a></li>
+                        <li class="active">Blogue</li>
+                    </ul>
+                </div>
+                <h3>Video</h3>
+            </div><!-- end section title -->
+        </section><!-- end section white -->
+
+        <!-- page builer -->
+
+        <section class="section-white clearfix">
+            <div class="container">
+                <div class="row">
+                    <div class="pull-left col-md-8 col-sm-8 col-xs-12">
+                        <div id="blog-page" class="row clearfix">
+                            <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn first">
+                               <div class="blog-item">
+                                            <div class="ImageWrapper">
+                                                <iframe width="560" height="315" src="https://www.youtube.com/embed/cf3mSsRAw8c" frameborder="0" allowfullscreen></iframe>
+                                            </div>
+                                            <div class="meta">
+                                                <span><a href="#">Cuisine</a> | 02-Jan-2015</span>
+                                            </div><!-- end meta -->
+                                            <div class="blog-title">
+                                                <h3><a href="blog-single-sidebar-one.html" title=""> Épisode du 16 février 2009</a></h3>
+                                            </div><!-- end title -->
+                                            <div class="blog-desc">
+                                                <p>Au menu cette semaine : les céréales santé</p>
+                                            </div><!-- end desc -->
+                                </div><!-- end blog -->
+                            </div><!-- end col -->
+
+                            <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn last">
+                                 <div class="blog-item">
+                                            <div class="ImageWrapper">
+                                                <iframe width="560" height="315" src="https://www.youtube.com/embed/kiFOyBknFKg" frameborder="0" allowfullscreen></iframe>
+                                            </div>
+                                            <div class="meta">
+                                                <span><a href="#">Cuisine</a> | 02-Jan-2015</span>
+                                            </div><!-- end meta -->
+                                            <div class="blog-title">
+                                                <h3><a href="blog-single-sidebar-one.html" title=""> Épisode du 9 septembre 2010</a></h3>
+                                            </div><!-- end title -->
+                                            <div class="blog-desc">
+                                                <p>Au menu cette semaine : remplacer la viande</p>
+                                            </div><!-- end desc -->
+                                </div><!-- end blog -->
+                            </div><!-- end col -->
+							
+							<div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn last">
+                                 <div class="blog-item">
+                                            <div class="ImageWrapper">
+                                                <iframe width="560" height="315" src="https://www.youtube.com/embed/09fqBP04rUQ" frameborder="0" allowfullscreen></iframe>
+                                            </div>
+                                            <div class="meta">
+                                                <span><a href="#">Cuisine</a> | 02-Jan-2015</span>
+                                            </div><!-- end meta -->
+                                            <div class="blog-title">
+                                                <h3><a href="blog-single-sidebar-one.html" title=""> Épisode du  17 novembre 2010</a></h3>
+                                            </div><!-- end title -->
+                                            <div class="blog-desc">
+                                                <p>Au menu cette semaine : les oeufs</p>
+                                            </div><!-- end desc -->
+                                </div><!-- end blog -->
+                            </div><!-- end col -->
+
+                      
+                        </div><!-- end blog -->
+
+                        <nav class="text-center"> 
+                            <ul class="pagination">
+                                <li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li>
+                                    <a href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div><!-- end pull-right -->
+
+                    <div class="pull-right col-md-4 col-sm-4 col-xs-12">
+                        <div id="sidebar" class="clearfix">
+                            <div class="widget">
+                                <div id="imaginary_container"> 
+                                    <div class="input-group stylish-input-group">
+                                        <input type="text" class="form-control"  placeholder="Rechercher" >
+                                        <span class="input-group-addon">
+                                            <button type="submit">
+                                                <span class="glyphicon glyphicon-search"></span>
+                                            </button>  
+                                        </span>
+                                    </div>
+                                </div>
+                            </div><!-- end widget -->
+
+
+                        <div class="widget wow fadeIn">
+                            <div class="widget-title">
+                                <h3>Posts récents</h3>
+                            </div><!-- end widget title -->
+                           <div class="featured-widget">
+                                <ul>
+                                    <li>
+                                        <img src="img/widget_07.jpg" alt="" class="alignleft">
+                                        <h3> <a href="#">20 façons simples d'économiser sur votre prochaine facture d'épicerie</a></h3>
+                                        <span class="metabox">
+                                            <a href="#">INFOS</a> <span>2 Janvier 2015</span>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <img src="img/widget_08.jpg" alt="" class="alignleft">
+                                        <h3> <a href="#">Dollarama et l’épicerie à très bas prix</a></h3>
+                                        <span class="metabox">
+                                            <a href="#">INFOS</a> <span>2 Janvier 2015</span>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <img src="img/widget_09.jpg" alt="" class="alignleft">
+                                        <h3> <a href="#">ENQUÊTE DE PRIX : NOS RÉSULTATS </a></h3>
+                                        <span class="metabox">
+                                            <a href="#">INFOS</a> <span>02 Janvier 2015</span>
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div><!-- end featured-widget -->
+                        </div><!-- end widget -->
+						
+                    </div><!-- end col -->
+                </div><!-- end sidebar -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </section><!-- end section white -->
+    
+
+    <!-- /#wrapper -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/retina.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.stellar.js"></script>
+    <script src="js/jquery.fitvids.js"></script>
+    <script src="js/home-scripts.js"></script>
+    <script>
+        $(document).ready(function () {
+            // Target your .container, .wrapper, .post, etc.
+            $(".blog-item").fitVids();
+        });
+    </script>
+    <!-- Portfolio -->
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/portfolio_01.js"></script>
+    <!-- Carousel -->
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/owl-scripts.js"></script>
+    <!-- FlexSlider Scripts-->
+    <script src="js/jquery.flexslider.js"></script>
+    <script type="text/javascript">
+        (function ($) {
+            "use strict";
+            $('.flexslider').flexslider({
+                animation: 'fade',
+                slideshow: true,
+                controlNav: false,
+                animationLoop: true,
+            });
+        })(jQuery);
+    </script>
+
+</body>
+</html>
---
+++ application/views/blog/press-release.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<!--<![endif]-->
+
+
+    
+    <!-- PORTFOLIO AND LIGHTBOX SETTINGS -->
+    <link href="<?php echo base_url("assets/blog/bfassets/css/bootFolio.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/blog/bfassets/css/prettyPhoto.css"); ?>" rel="stylesheet">
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url("assets/blog/css/font-awesome.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/blog/css/et-icons.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/blog/css/animate.min.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/blog/css/owl-carousel.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/blog/css/flexslider.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/blog/style.css"); ?>" rel="stylesheet">
+    <!-- Custom font -->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+
+
+    <!-- page builer -->
+
+    <section class="section-white clearfix blog" ng-controller="AccountController">
+        <div class="container">
+            <div class="row">
+                <div class="pull-left col-md-8 col-sm-8 col-xs-12">
+                    <div id="blog-page" class="row clearfix">
+                        <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn first">
+                            <div class="blog-item">
+                                <div class="ImageWrapper">
+                                    <img src="<?php echo base_url("assets/blog/"); ?>img/epicerie.png" alt="" class="img-responsive">
+                                    <div class="ImageOverlayLi"></div>
+                                    <div class="Buttons StyleH">
+                                        <a href="#" title="Like it"><span class="bubble border-radius"><i class="fa fa-heart-o"></i> 3</span></a>
+                                        <a href="blog-20 façons simples d'économiser.html" title="Permalink to : My post example title goes here"><span class="bubble border-radius"><i class="fa fa-comment-o"></i> 7</span></a>
+                                    </div>
+                                </div>
+                                <div class="meta">
+                                    <span><a href="#">INFOS</a> | 02-Jan-2015</span>
+                                </div><!-- end meta -->
+                                <div class="blog-title">
+                                    <h3><a href="blog-2economiser.html" title="">20 façons simples d'économiser sur votre prochaine facture d'épicerie</a></h3>
+                                </div><!-- end title -->
+                                <div class="blog-desc">
+                                    <p>Faire son épicerie, ça coûte cher. Il est tellement facile d’y entrer pour quelques produits et d’y ressortir avec un portefeuille vide. Puisque nous devons tous nous nourrir, pas le choix, il vaut mieux privilégier les repas faits maison, qui coûtent moins cher et sont plus nourrissants.</p>
+                                </div><!-- end desc -->
+                                <div class="blog-button">
+                                    <a href="blog-economiser.html" title="" class="btn btn-primary border-radius">Lire</a>
+                                </div><!-- end button -->
+                            </div><!-- end blog -->
+                        </div><!-- end col -->
+
+                        <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn last">
+                            <div class="blog-item">
+                                <div class="ImageWrapper">
+                                    <img src="<?php echo base_url("assets/blog/"); ?>img/epicerie2.png" alt="" class="img-responsive">
+                                    <div class="ImageOverlayLi"></div>
+                                    <div class="Buttons StyleH">
+                                        <a href="#" title="Like it"><span class="bubble border-radius"><i class="fa fa-heart-o"></i> 3</span></a>
+                                        <a href="blog-dollarama.html" title="Permalink to : My post example title goes here"><span class="bubble border-radius"><i class="fa fa-comment-o"></i> 7</span></a>
+                                    </div>
+                                </div>
+                                <div class="meta">
+                                    <span><a href="#">INFOS</a> | 02-Jan-2015</span>
+                                </div><!-- end meta -->
+                                <div class="blog-title">
+                                    <h3><a href="blog-single-sidebar-one.html" title="">Dollarama et l’épicerie à très bas prix</a></h3>
+                                </div><!-- end title -->
+                                <div class="blog-desc">
+                                    <p>Alors que le prix d’un panier type d’épicerie a grimpé de près de 12 % en deux ans, Le Journal a constaté que les consommateurs peuvent économiser de 32 % à 45 % s’ils font une partie de leur épicerie chez... Dollarama. Sur une liste de 11 articles, identiques ou équivalents selon le commerce, la facture totale varie entre 23,50 $ chez Dollarama, et 43,19 $ chez Provigo. Si plusieurs consommateurs croient qu’il n’y a que du «cannage» et des bonbons au Dollarama, détrompez-vous. De nombreux produits de marque, également offerts dans les supermarchés, s’y trouvent à des prix jusqu’à deux fois moins élevés. Il y a notamment du beurre d’arachides naturel, de l’édulcorant à base de feuilles de stévia et du gruau de marque connue. Offre complémentaire. </p>
+                                </div><!-- end desc -->
+                                <div class="blog-button">
+                                    <a href="blog-dollarama.html" title="" class="btn btn-primary border-radius">Lire</a>
+                                </div><!-- end button -->
+                            </div><!-- end blog -->
+                        </div><!-- end col -->
+
+                        <div class="col-md-12 col-sm-12 col-xs-12 wow fadeIn first">
+                            <div class="blog-item">
+                                <div class="ImageWrapper">
+                                    <img src="<?php echo base_url("assets/blog/"); ?>img/epicerie3.jpg" alt="" class="img-responsive">
+                                    <div class="ImageOverlayLi"></div>
+                                    <div class="Buttons StyleH">
+                                        <a href="#" title="Like it"><span class="bubble border-radius"><i class="fa fa-heart-o"></i> 3</span></a>
+                                        <a href="blog-enquete.html" title="Permalink to : My post example title goes here"><span class="bubble border-radius"><i class="fa fa-comment-o"></i> 7</span></a>
+                                    </div>
+                                </div>
+                               <div class="meta">
+                                            <span><a href="#">INFOS</a> | 02-Jan-2015</span>
+                                        </div><!-- end meta -->
+                                        <div class="blog-title">
+                                            <h3><a href="blog-single-sidebar-one.html" title="">ENQUÊTE DE PRIX : NOS RÉSULTATS</a></h3>
+                                        </div><!-- end title -->
+                                        <div class="blog-desc">
+                                            <p>Dans quelle épicerie et dans quelle région sont les aliments les moins chers? Est-il avantageux de courir les rabais de supermarché en supermarché? Les marques maison en valent-elles toujours la peine? Voici ce que révèle notre enquête. Par un heureux hasard, IGA a lancé sa campagne de changement de prix quelques jours après le début de notre grand magasinage, ce qui nous a permis de faire d’intéressantes comparaisons. Nos premières visites ont eu lieu avant la réduction annoncée, et les autres, après. En moyenne, le prix du panier contenant des produits de marques nationales a diminué de 7$ après l’annonce, et celui des marques économiques, de 14$.</p>
+                                        </div><!-- end desc -->
+                                        <div class="blog-button">
+                                            <a href="blog-enquete.html" title="" class="btn btn-primary border-radius">Lire</a>
+                                        </div><!-- end button -->
+                            </div><!-- end blog -->
+                        </div><!-- end col -->
+                    </div><!-- end blog -->
+
+                    <nav class="text-center"> 
+                        <ul class="pagination">
+                            <li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+                            <li><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li>
+                                <a href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div><!-- end pull-right -->
+
+                <div class="pull-right col-md-4 col-sm-4 col-xs-12">
+                    <div id="sidebar" class="clearfix">
+                        <div class="widget">
+                            <div id="imaginary_container" class="col-md-12"> 
+                                <div class="input-group stylish-input-group">
+                                    <md-input-container class="col-md-12">
+                                        <input aria-label="Search" ng-model="recentPostsSearchText">
+                                        <md-icon style="color: #1abc9c;"><i class="material-icons">search</i></md-icon>
+                                    </md-input-container>
+                                </div>
+                            </div>
+                        </div><!-- end widget -->
+
+
+                      <div class="widget wow fadeIn">
+                        <div class="widget-title">
+                            <h3>Posts récents</h3>
+                        </div><!-- end widget title -->
+                       <div class="featured-widget">
+                            <ul>
+                                <li>
+                                    <img src="<?php echo base_url("assets/blog/"); ?>img/widget_07.jpg" alt="" class="alignleft">
+                                    <h3> <a href="#">20 façons simples d'économiser sur votre prochaine facture d'épicerie</a></h3>
+                                    <span class="metabox">
+                                        <a href="#">INFOS</a> <span>2 Janvier 2015</span>
+                                    </span>
+                                </li>
+                                <li>
+                                    <img src="<?php echo base_url("assets/blog/"); ?>img/widget_08.jpg" alt="" class="alignleft">
+                                    <h3> <a href="#">Dollarama et l’épicerie à très bas prix</a></h3>
+                                    <span class="metabox">
+                                        <a href="#">INFOS</a> <span>2 Janvier 2015</span>
+                                    </span>
+                                </li>
+                                <li>
+                                    <img src="<?php echo base_url("assets/blog/"); ?>img/widget_09.jpg" alt="" class="alignleft">
+                                    <h3> <a href="#">ENQUÊTE DE PRIX : NOS RÉSULTATS </a></h3>
+                                    <span class="metabox">
+                                        <a href="#">INFOS</a> <span>02 Janvier 2015</span>
+                                    </span>
+                                </li>
+                            </ul>
+                        </div><!-- end featured-widget -->
+                    </div><!-- end widget -->
+
+                </div><!-- end col -->
+            </div><!-- end sidebar -->
+        </div><!-- end row -->
+    </div><!-- end container -->
+</section><!-- end section white -->
+
+    <script src="<?php echo base_url("assets/blog/js/retina.js"); ?>"></script>
+    <script src="<?php echo base_url("assets/blog/"); ?>js/wow.min.js"></script>
+    <script src="<?php echo base_url("assets/blog/"); ?>js/jquery.stellar.js"></script>
+    <script src="<?php echo base_url("assets/blog/"); ?>js/jquery.fitvids.js"></script>
+    
+    <script>
+        $(document).ready(function () {
+            // Target your .container, .wrapper, .post, etc.
+            $(".blog-item").fitVids();
+        });
+    </script>
+    
+    <!-- Portfolio -->
+    <script src="<?php echo base_url("assets/blog/js/jquery.isotope.min.js"); ?>"></script>
+    <script src="<?php echo base_url("assets/blog/js/portfolio_01.js"); ?>"></script>
+    <!-- Carousel -->
+    <script src="<?php echo base_url("assets/blog/js/owl.carousel.js"); ?>"></script>
+    <script src="<?php echo base_url("assets/blog/js/owl-scripts.js"); ?>"></script>
+    <!-- FlexSlider Scripts-->
+    <script src="<?php echo base_url("assets/blog/js/jquery.flexslider.js"); ?>"></script>
+    <script type="text/javascript">
+        (function ($) {
+            "use strict";
+            $('.flexslider').flexslider({
+                animation: 'fade',
+                slideshow: true,
+                controlNav: false,
+                animationLoop: true
+            });
+        })(jQuery);
+    </script>
