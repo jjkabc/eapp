f69040697eb0128e8c3c9e6bf67328973b6dd83f
Samuel Besong <besongsamuel@yahoo.com>
2017-08-04 14:19:15 -0400

    Update blog-controller.js
    Added methods to like and dislike

--- assets/js/blog-controller.js
+++ assets/js/blog-controller.js
@@ -4,7 +4,55 @@
  * and open the template in the editor.
  */
 
-angular.module("eappApp").controller("BlogController", ["$rootScope", "$http", function($rootScope, $http) 
+angular.module("eappApp").controller("BlogController", ["$scope", "$http", function($scope, $http) 
 {
+	
+	/*
+	* This function increments the number of likes for a given post
+	* for a given user. 
+	*/
+	$scope.like = function(post_id)
+	{
+		var formdata = new FormData();
+		formdata.append("post_id", post_id);
+		
+		$http.post( $scope.site_url.concat("/blog/like"), 
+		formdata, { transformRequest: angular.identity, headers: {'Content-Type': undefined}}).then(
+		function(response)
+		{
+			var likes = response.data.likes;
+			$scope.RecentPosts[post_id].likes = likes;
+		});
+	};
+	
+	$scope.dislike = function(post_id)
+	{
+		var formdata = new FormData();
+		formdata.append("post_id", post_id);
+		
+		$http.post( $scope.site_url.concat("/blog/dislike"), 
+		formdata, { transformRequest: angular.identity, headers: {'Content-Type': undefined}}).then(
+		function(response)
+		{
+			var likes = response.data.likes;
+			$scope.RecentPosts[post_id].likes = likes;
+		});
+	};
+	
+	$scope.canLike(post_id)
+	{
+		for(var i in $scope.RecentPosts[post_id].likes)
+		{
+			var like = $scope.RecentPosts[post_id].likes[i];
+			
+			if(parseInt(like.user_account_id) === parseInt(loggedUser.id))
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
 	
 }]);
