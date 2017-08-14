77ac7b122e6efddd2c6a7e93e8ab637b9b58b65a
Samuel Besong <besongsamuel@yahoo.com>
2017-08-14 16:26:27 -0400

    Update stat-detail.php

--- application/views/blog/stat-detail.php
+++ application/views/blog/stat-detail.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
+<div class="product-big-title-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="product-bit-title text-center">
+                    <h2>Details</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End Page title area -->
 
 <section id="blog-container" class="section-white clearfix" ng-controller="BlogController">
     <div class="container">
